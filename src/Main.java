c052112bce0c7e235b584b09140e4db73cff9630
Ekaterina Botova <ksergeevnak@yandex.ru>
2022-08-19 12:51:57 +0300

    added the average value

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = { 40, 4, 20, 80};
         SalesManager manager = new SalesManager(sales);
-        System.out.println("Max : " + manager.max());
+
+        System.out.println("averageValue : " + manager.averageValue());
 
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,19 +1,39 @@
+import java.util.Arrays;
 
-    public class SalesManager {
-        protected int[] sales;
+public class SalesManager {
+    protected int[] sales;
 
-        public SalesManager(int[] sales) {
-            this.sales = sales;
-        }
-
-        public int max() {
-            int max = -1;
-            for (int sale : sales) {
-                if (sale > max) {
-                    max = sale;
-                }
-            }
-            return max;
-        }
+    public SalesManager(int[] sales) {
+        this.sales = sales;
     }
+
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+
+    }
+
+    public int min() {
+        int min = 1;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+
+            }
+        }
+        return min;
+    }
+    public int averageValue () {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return (sum - min() - max()) / (sales.length -2);
+    }
+}
 
